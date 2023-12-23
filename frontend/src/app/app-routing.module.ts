ef3e5d939df24a9d01b16fc72cffe3e345722a3f
noemiegs <113913066+noemiegs@users.noreply.github.com>
2023-12-24 00:44:59 +0100

    new commit

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DeckComponent } from './deck/deck.component';
+import { AddComponent } from './add/add.component';
+import { BrowseComponent } from './browse/browse.component';
+import { StatsComponent } from './stats/stats.component';
 
-const routes: Routes = [];
+const routes: Routes = [  { path: 'deck', component: DeckComponent },
+{ path: 'add', component: AddComponent },
+{ path: 'browse', component: BrowseComponent },
+{ path: 'stats', component: StatsComponent },
+{ path: '', redirectTo: '/deck', pathMatch: 'full' } // default route
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DeckComponent } from './deck/deck.component';
+import { AddComponent } from './add/add.component';
+import { BrowseComponent } from './browse/browse.component';
+import { StatsComponent } from './stats/stats.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DeckComponent,
+    AddComponent,
+    BrowseComponent,
+    StatsComponent
   ],
   imports: [
     BrowserModule,
