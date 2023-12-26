0f4ab9b52b8d1b22cd0f154ca84092433e7486bd
noemiegs <113913066+noemiegs@users.noreply.github.com>
2023-12-26 22:48:13 +0100

    logo added

--- frontend/src/app/add/add.component.ts
+++ frontend/src/app/add/add.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./add.component.css']
 })
 export class AddComponent {
+  public recto: string;
+  public verso: string;
 
+  constructor() {
+    this.recto = '';
+    this.verso = '';
+  }
+
+  public updateInputs() {
+    console.log(this.recto, this.verso);
+  }
+
+  public addFunction(): void {
+    console.log('Bouton Add cliqué');
+    // Ici, vous pourriez ajouter des éléments à une liste, ouvrir un formulaire, etc.
+  }
 }
