0b325ad35d95923f05933fa3a2803269edd056d5
sylverjoy <joyyamsi6@gmail.com>
2023-12-28 06:51:38 +0100

    Fixing DB Conection

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,21 @@
 import express from 'express';
 import routes from './routes/routes'; // ajustez le chemin selon votre structure de dossiers
+import { sequelize } from './config/database';
 
 const app = express();
 
 app.use(express.json());
+
+async function testDB() {
+  try {
+    await sequelize.authenticate();
+    console.log('DB connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+};
+
+testDB();
 
 // Utiliser le router pour toutes les routes
 app.use('/', routes);
--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -4,11 +4,12 @@ import { Sequelize } from 'sequelize';
 const dbName = process.env.DB_NAME || 'mydb';
 const dbUser = process.env.DB_USER || 'user';
 const dbPass = process.env.DB_PASS || 'password';
-const dbHost = process.env.DB_HOST || 'localhost';
+const dbHost = process.env.DB_HOST || 'db';
 
 // Création de l'instance Sequelize
 const sequelize = new Sequelize(dbName, dbUser, dbPass, {
   host: dbHost,
+  port: 5432,
   dialect: 'postgres',  // Utiliser le dialecte PostgreSQL
   logging: false,       // Désactiver la journalisation pour une sortie plus propre
   // Autres options de configuration selon les besoins
