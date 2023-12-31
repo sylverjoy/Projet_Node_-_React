392dc5ebbdbe9423a560f406a8fcd41e9377c86e
sylverjoy <joyyamsi6@gmail.com>
2023-12-31 19:39:10 +0100

    final touches

--- frontend/src/app/card/card.component.ts
+++ frontend/src/app/card/card.component.ts
@@ -35,14 +35,43 @@ export class CardComponent implements OnInit {
             });
           };
           if (deck[0].Cards) {
-            const today = new Date();
-            this.cards = deck[0].Cards.filter((card:any) => {
-              return !card.lastReviewedDate || new Date(card.nextReviewDate) <= today;
-            });
+            const deckcards = deck[0].Cards;
+            let filteredArray: any[] = [];
+            let completedRequests = 0;
+          
+            for (let i = 0; i < deckcards.length; i++) {
+              this.apiService.getCardById(deckcards[i].id).subscribe(
+                (card: any) => {
+                  if (card[0].UserCards[0].numberOfTimesReviewed === 0 || card[0].UserCards[0].nextReviewDate <= new Date() || card[0].UserCards[0].confidenceLevel == "Again") {
+                    filteredArray.push(card);
+                  }
+                  completedRequests++;
+          
+                  if (completedRequests === deckcards.length) {
+                    // All requests are completed
+                    this.cards = filteredArray;
+                    console.log(this.cards);
+                  }
+                }
+                
+                ,
+                (error: any) => {
+                  console.error('Error fetching cards:', error);
+                  completedRequests++;
+          
+                  if (completedRequests === deckcards.length) {
+                    // All requests are completed
+                    this.cards = filteredArray;
+                    console.log(this.cards);
+                  }
+                }
+              );
+            }
           } else {
-            this.cards = []; 
+            this.cards = [];
             console.log('Aucune carte trouvée ou deck est indéfini');
           }
+          
         });
       }
     });
@@ -58,7 +87,7 @@ export class CardComponent implements OnInit {
     if (this.isDeckCompleted) {
       return;
     }
-    this.apiService.getCardById(this.cards[this.currentCardIndex].id).subscribe((card: any) => {
+    this.apiService.getCardById(this.cards[this.currentCardIndex][0].id).subscribe((card: any) => {
       //Create UserCard if not created
       if (card[0].UserCards.length == 0) {
         this.apiService.createUserCard({
@@ -73,7 +102,8 @@ export class CardComponent implements OnInit {
       switch(level) {
         case 'Again':
           card[0].UserCards[0].nextReviewDate = new Date();
-          //this.cards.push(card);
+          //const arr: any[] = [card]
+          //this.cards.push(arr);
           break;
         case 'Hard':
           card[0].UserCards[0].nextReviewDate = new Date(new Date().getTime() + (2 * 1000 * 60 * 60 * 24));
